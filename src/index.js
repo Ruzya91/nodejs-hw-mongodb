50bf779a78306651d16feb1e8bbd9225d249e0ad
Ruzanna <pogosian.ruz@gmail.com>
2025-07-13 10:33:19 +0200

    +

--- src/index.js
+++ src/index.js
@@ -1,26 +1,18 @@
 import express from 'express';
 import cors from 'cors';
-// import {
-//   handleGetContactById,
-//   handleGetAllContacts,
-// } from './controllers/contactsController.js';
+
 import contactsRouter from './routers/contacts.js';
 import notFoundHandler from './middlewares/notFoundHandler.js';
 import errorHandler from './middlewares/errorHandler.js';
 
 const app = express();
-console.log('23');
+app.get('/', (req, res) => {
+  res.json({ message: 'API is running' });
+});
+
 app.use(cors());
 app.use(express.json());
 
-// // === Routes ===
-// app.get('/contacts/:contactId', handleGetContactById);
-// app.get('/contacts', handleGetAllContacts);
-
-// 404 handler
-// app.use((req, res) => {
-//   res.status(404).json({ message: 'Not found' });
-// });
 app.use('/contacts', contactsRouter);
 app.use(notFoundHandler);
 app.use(errorHandler);

2d9f5d0fb3bce6b5ec2b9fd4d172c6203c26d3e8
Ruzanna <pogosian.ruz@gmail.com>
2025-07-24 21:08:58 +0200

    +

--- src/controllers/contactsController.js
+++ src/controllers/contactsController.js
@@ -9,7 +9,8 @@ import {
 
 // GET /contacts
 export const handleGetAllContacts = async (req, res) => {
-  const userId = req.user._id;
+  console.log('💡 GET /contacts hit');
+  const userId = '686ee4c5e8239f8c2765728a';
 
   const page = parseInt(req.query.page) || 1;
   const perPage = parseInt(req.query.perPage) || 10;
@@ -19,7 +20,7 @@ export const handleGetAllContacts = async (req, res) => {
   const isFavourite = req.query.isFavourite;
 
   const filters = {
-    userId, // 🔐 фільтрація по користувачу
+    userId,
     ...(type && { contactType: type }),
     ...(typeof isFavourite !== 'undefined' && { isFavourite }),
   };
@@ -52,7 +53,7 @@ export const handleGetAllContacts = async (req, res) => {
 // GET /contacts/:contactId
 export const handleGetContactById = async (req, res) => {
   const { contactId } = req.params;
-  const userId = req.user._id;
+  const userId = '686ee4c5e8239f8c2765728a';
 
   const contact = await getContactById(contactId, userId); // 🔐 тільки свої контакти
 
@@ -69,7 +70,7 @@ export const handleGetContactById = async (req, res) => {
 
 // POST /contacts
 export const handleCreateContact = async (req, res) => {
-  const userId = req.user._id;
+  const userId = '686ee4c5e8239f8c2765728a';
   const newContact = await createContact({ ...req.body, userId }); // 🔐 додаємо userId
 
   res.status(201).json({
@@ -82,7 +83,7 @@ export const handleCreateContact = async (req, res) => {
 // PATCH /contacts/:contactId
 export const handlePatchContact = async (req, res) => {
   const { contactId } = req.params;
-  const userId = req.user._id;
+  const userId = '686ee4c5e8239f8c2765728a';
 
   const updatedContact = await updateContactById(contactId, req.body, userId); // 🔐 з userId
 
@@ -100,7 +101,7 @@ export const handlePatchContact = async (req, res) => {
 // DELETE /contacts/:contactId
 export const handleDeleteContact = async (req, res) => {
   const { contactId } = req.params;
-  const userId = req.user._id;
+  const userId = '686ee4c5e8239f8c2765728a';
 
   const deletedContact = await deleteContactById(contactId, userId); // 🔐 з userId
 
